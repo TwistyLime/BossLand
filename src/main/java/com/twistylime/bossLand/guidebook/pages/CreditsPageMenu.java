d81803d3d389e0db98d6057cd36736a94a5d2e42
Nitesh <niteshmali132002@gmail.com>
2025-09-01 13:36:25 +0530

    Implemented info and credit page in guide book

--- src/main/java/com/twistylime/bossLand/guidebook/pages/CreditsPageMenu.java
+++ src/main/java/com/twistylime/bossLand/guidebook/pages/CreditsPageMenu.java
@@ -2,8 +2,16 @@ package com.twistylime.bossLand.guidebook.pages;
 
 import com.twistylime.bossLand.guidebook.PaginatedMenu;
 import com.twistylime.bossLand.guidebook.menuutility.PlayerMenuUtility;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CreditsPageMenu extends PaginatedMenu {
     public CreditsPageMenu(PlayerMenuUtility playerMenuUtility) {
@@ -31,8 +39,9 @@ public class CreditsPageMenu extends PaginatedMenu {
                 case BARRIER:
                     e.getWhoClicked().closeInventory();
                 case NAME_TAG:
-                    e.getWhoClicked().sendMessage("Need to implement search functionality");
+                    e.getWhoClicked().sendMessage("This feature is work in progress and will be available in the upcoming updates.");
                     e.getWhoClicked().closeInventory();
+                    break;
                 default:
                     break;
             }
@@ -41,6 +50,31 @@ public class CreditsPageMenu extends PaginatedMenu {
 
     @Override
     public void setMenuItems() {
-        addSmallMenuBorder();
+        addSmallCreditMenuBorder();
+
+        List<ItemStack> credits = new ArrayList<>();
+
+        Map<String, List<String>> creditData = new LinkedHashMap<>();
+        creditData.put(ChatColor.GOLD + "" + ChatColor.BOLD + "Original Developer",
+                List.of(ChatColor.YELLOW + "Eliminator"));
+        creditData.put(ChatColor.AQUA + "" + ChatColor.BOLD + "Current Developer",
+                List.of(ChatColor.GREEN + "TwistyLime"));
+        creditData.put(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "License",
+                List.of(ChatColor.WHITE + "MIT License"));
+
+        int[] slots = {11, 13, 15};
+        int index = 0;
+
+        for (Map.Entry<String, List<String>> entry : creditData.entrySet()) {
+            ItemStack newCredit = new ItemStack(Material.NETHER_STAR, 1);
+            ItemMeta meta = newCredit.getItemMeta();
+            if (meta != null) {
+                meta.setDisplayName(entry.getKey());
+                meta.setLore(entry.getValue());
+                newCredit.setItemMeta(meta);
+            }
+            credits.add(newCredit);
+            inventory.setItem(slots[index++], newCredit);
+        }
     }
 }
--- src/main/java/com/twistylime/bossLand/guidebook/pages/InfoPageMenu.java
+++ src/main/java/com/twistylime/bossLand/guidebook/pages/InfoPageMenu.java
@@ -2,8 +2,19 @@ package com.twistylime.bossLand.guidebook.pages;
 
 import com.twistylime.bossLand.guidebook.PaginatedMenu;
 import com.twistylime.bossLand.guidebook.menuutility.PlayerMenuUtility;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class InfoPageMenu extends PaginatedMenu {
 
@@ -32,8 +43,15 @@ public class InfoPageMenu extends PaginatedMenu {
                 case BARRIER:
                     e.getWhoClicked().closeInventory();
                 case NAME_TAG:
-                    e.getWhoClicked().sendMessage("Need to implement search functionality");
+                    e.getWhoClicked().sendMessage("This feature is work in progress and will be available in the upcoming updates.");
                     e.getWhoClicked().closeInventory();
+                    break;
+                case NETHER_STAR:
+                    TextComponent message = new TextComponent("§a§lClick here to open the plugin page!");
+                    message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.spigotmc.org/resources/boss-land.127099/"));
+                    e.getWhoClicked().spigot().sendMessage(message);
+                    e.getWhoClicked().closeInventory();
+                    break;
                 default:
                     break;
             }
@@ -42,6 +60,31 @@ public class InfoPageMenu extends PaginatedMenu {
 
     @Override
     public void setMenuItems() {
-        addSmallMenuBorder();
+        addSmallCreditMenuBorder();
+
+        List<ItemStack> credits = new ArrayList<>();
+
+        Map<String, List<String>> creditData = new LinkedHashMap<>();
+        creditData.put(ChatColor.GOLD + "" + ChatColor.BOLD + "Plugin Version",
+                List.of(ChatColor.YELLOW + "BossLand v2.3"));
+        creditData.put(ChatColor.AQUA + "" + ChatColor.BOLD + "Supported Minecraft Versions",
+                List.of(ChatColor.GREEN + "v1.16.1 - v1.21.5"));
+        creditData.put(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Visit Plugin Page",
+                List.of(ChatColor.WHITE + "MIT License"));
+
+        int[] slots = {11, 13, 15};
+        int index = 0;
+
+        for (Map.Entry<String, List<String>> entry : creditData.entrySet()) {
+            ItemStack newCredit = new ItemStack(Material.NETHER_STAR, 1);
+            ItemMeta meta = newCredit.getItemMeta();
+            if (meta != null) {
+                meta.setDisplayName(entry.getKey()); // bold colored title
+                meta.setLore(entry.getValue());     // description as lore
+                newCredit.setItemMeta(meta);
+            }
+            credits.add(newCredit);
+            inventory.setItem(slots[index++], newCredit);
+        }
     }
 }
